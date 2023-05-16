70a62aece31cd5666713750e58be3eb50e65728e
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-05-16 12:38:01 +0800

    check.go 做了正则表达式修正 check_test.go 补充单元测试

--- check.go
+++ check.go
@@ -1,31 +1,54 @@
 package tools
 
 import (
-	"errors"
 	"fmt"
 	"regexp"
 	"strings"
 )
 
-// 中文检查
-func CheckChinese(input *string) error {
+// CheckChinese 中文检查
+// 仅允许中文、英文字母、数字和空白字符，和常见标点符号，过滤特殊字符，特别是sql注入
+func CheckChinese(input *string) bool {
 	// 去除首位空格
 	userInput := strings.TrimSpace(*input)
-	// 限制输入中只能出现指定字符集合[\u4e00-\u9fa5\d\s]+
-	if ok, err := regexp.MatchString("^[a-zA-Z0-9\u4e00-\u9fa5\\s]+$", userInput); !ok {
+	if ok, err := regexp.MatchString(`^([\p{L}\p{N}\s.,;：'！？。]|\p{Han})+$`, userInput); !ok {
 		fmt.Println(err, ok)
-		return errors.New("仅允许中文、英文字母、数字和空白字符")
+		return false
 	}
-	return nil
+	return true
 }
 
 // 手机号码检查
-func CheckPhoneNumber(input *string) error {
+func CheckPhoneNumber(input *string) bool {
 	// 去除首位空格
 	userInput := strings.TrimSpace(*input)
-	// 手机号码检查
-	if ok, _ := regexp.MatchString(`^1(3\d|4[57]|5[^4\D]|6[67]|7[^249\D]|8\d|9[89])\d{8}$`, userInput); !ok {
-		return errors.New(" 是非法的手机号")
+	re1 := regexp.MustCompile(`^\+?[\d\s-]+$`)
+	re2 := regexp.MustCompile(`^\+\d{1,2}\s?\(\d{1,3}\)\s?\d{1,4}[\s-]?\d{1,4}[\s-]?\d{1,9}$`)
+	re3 := regexp.MustCompile(`^\+\d{1,2}\s?\d{1,4}[\s-]?\d{1,4}[\s-]?\d{1,9}$`)
+	re4 := regexp.MustCompile(`^(\+\d{1,2}\s?)?(\(\d{1,3}\)\s?)?(\d{1,4}[\s-]?)?\d{1,4}[\s-]?\d{1,9}$`)
+
+	// 第一步基本格式校验
+	ok1 := re1.MatchString(userInput)
+	if !ok1 {
+		return false
 	}
-	return nil
+
+	// 第二步更具体的电话号码格式校验
+	ok2 := re2.MatchString(userInput)
+	ok3 := re3.MatchString(userInput)
+	if !(ok2 || ok3) {
+		// 不符合带括号的格式，再进行位数校验
+		ok4 := re4.MatchString(userInput)
+		if !ok4 {
+			return false
+		}
+		// 进行位数校验，此处假设电话号码的有效位数范围是10-15
+		numberOnly := regexp.MustCompile(`\d`)
+		digits := numberOnly.FindAllString(userInput, -1)
+		if len(digits) < 10 || len(digits) > 15 {
+			return false
+		}
+	}
+
+	return true
 }
---
+++ check_test.go
@@ -0,0 +1,77 @@
+package tools
+
+import "testing"
+
+func TestCheckChinese(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult bool
+	}{
+		{"test1", args{input: "s"}, true},
+		{"test2", args{input: "测试"}, true},
+		{"test3", args{input: "Hello, 世界"}, true},
+		{"test4", args{input: "12345"}, true},
+		{"test5", args{input: "中文校验函数"}, true},
+		{"test6", args{input: "!@#$%^&*()_+"}, false},
+		{"test7", args{input: "This is a test."}, true},
+		{"test8", args{input: "123abcDEF"}, true},
+		{"test9", args{input: "空白字符 \t \n"}, true},
+		{"test10", args{input: "常见标点符号：！？。"}, true},
+		{"sql 注入 1", args{input: "1 OR 1=1; --"}, false},
+		{"sql 注入 2", args{input: "SELECT * FROM users WHERE username='admin' OR 1=1; --"}, false},
+		{"sql 注入 3", args{input: "DROP TABLE users; --"}, false},
+		{"sql 注入 4", args{input: "INSERT INTO users (username, password) VALUES ('admin', 'password'); --"}, false},
+		{"sql 注入 5", args{input: "UPDATE users SET password='newpassword' WHERE username='admin'; --"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if result := CheckChinese(&tt.args.input); result != tt.wantResult {
+				t.Errorf("CheckChinese() result = %v, wantResult %v", result, tt.wantResult)
+			}
+		})
+	}
+}
+
+func TestCheckPhoneNumber(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantResult bool
+	}{
+		{"合法电话号码-纯数字", args{input: "1234567890"}, true},
+		{"合法电话号码-带横线", args{input: "123-456-7890"}, true},
+		{"合法电话号码-带括号", args{input: "(123) 456-7890"}, true},
+		{"合法电话号码-国际格式", args{input: "+1 123-456-7890"}, true},
+		{"非法电话号码-不完整", args{input: "123456789"}, false},
+		{"非法电话号码-包含字母", args{input: "abcdefghij"}, false},
+		{"非法电话号码-错误分隔符", args{input: "12-34-567-890"}, false},
+		{"非法电话号码-非法字符", args{input: "1.800.123.4567"}, false},
+		{"非法电话号码-括号不匹配", args{input: "+1 (123) 456-7890"}, false},
+		{"合法电话号码-特殊字符空格", args{input: "123 456 7890"}, true},
+		{"不支持电话号码-特殊字符点号", args{input: "123.456.7890"}, false},
+		{"不支持电话号码-特殊字符斜杠", args{input: "123/456/7890"}, false},
+		{"不支持电话号码-特殊字符下划线", args{input: "123_456_7890"}, false},
+		{"合法电话号码-带国家代码", args{input: "+86 10 12345678"}, true},
+		{"非法电话号码-空字符串", args{input: ""}, false},
+		{"非法电话号码-包含特殊符号", args{input: "!@#$%^&*()"}, false},
+		//此测试点是因为对手机号码做了格式化
+		{"合法电话号码-带有空格", args{input: "1234567 890"}, true},
+		{"非法电话号码-太长的号码", args{input: "1234567890123456789012345678901234567890"}, false},
+		{"合法电话号码-带加号", args{input: "+1234567890"}, true},
+		{"合法电话号码-带国际长途拨号前缀", args{input: "0111234567890"}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if result := CheckPhoneNumber(&tt.args.input); result != tt.wantResult {
+				t.Errorf("CheckPhoneNumber() result = %v, wantResult %v", result, tt.wantResult)
+			}
+		})
+	}
+}
